2817a14840820bc91e980994d7cc9442dcc57537
aamuzakii <aamuzakii@gmail.com>
2022-05-20 13:20:43 +0700

    Card horizontal view

--- src/components/home/OrderCard.jsx
+++ src/components/home/OrderCard.jsx
@@ -4,16 +4,18 @@ import { Subheading, Button, Searchbar, Caption  } from 'react-native-paper';
 
 export default function OrderCard() {
   let screenHeight = Dimensions.get('window').height;
+  let screenWidth = Dimensions.get('window').width;
 
   return (
-    <View style={{ height: 90, backgroundColor: 'white', margin: 5, borderRadius: 5, borderColor: '#f4f4f4', borderWidth: 3, shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 3,
-    },
-    shadowOpacity: 0.27,
-    shadowRadius: 4.65, }} >
-      <Text>JJJ8888888888888J</Text>
+    <View style={{ height: 190, width: screenWidth * 75 / 100, backgroundColor: 'white', margin: 5, borderRadius: 10, borderColor: '#f4f4f4', borderWidth: 3, shadowColor: "#000", shadowOffset: { width: 0, height: 3 }, shadowOpacity: 0.27, shadowRadius: 4.65 }} >
+      <View id="atas" >
+        <View>
+          <Text>Zaki</Text>
+          <Text style={{ fontSize: 12 }} >05 Mei 2022, 01: 33 PM</Text>
+        </View>
+        <View></View>
+      </View>
+      <View id="atas" ></View>
     </View>
   );
 }
--- src/views/HomeScreen.jsx
+++ src/views/HomeScreen.jsx
@@ -12,7 +12,7 @@ const Home = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={{ backgroundColor: '#f4f6f8', flexGrow: 1, padding: 10 }} >
+      <View style={{ backgroundColor: '#f4f6f8', flexGrow: 1 }} >
         <Subheading >Tombol Kilat</Subheading>
         <View style={{ flexDirection: 'row', justifyContent: 'space-around' }} >
           {/* https://materialdesignicons.com/ */}
@@ -21,9 +21,11 @@ const Home = () => {
         </View>
       </View>
       {/* SCROLL SECTION */}
-      <View style={{ height: (screenHeight - 100), borderColor: 'green', paddingHorizontal: 20 }} >
+      <View style={{ height: (screenHeight - 100), borderColor: 'green' }} >
         <ScrollView >
           <Statistic></Statistic>
+
+          {/* component share link */}
           <View>
             <Caption>Bagikan link ke pelanggan</Caption>
             <View style={{ flexDirection: 'row',  justifyContent: 'space-around' }} >
@@ -41,13 +43,15 @@ const Home = () => {
             </View>
           </View>
 
-          <ScrollView horizontal={true}>
+          <ScrollView horizontal={true} style={{ backgroundColor: 'white', paddingHorizontal: 20, marginVertical: 15 }}>
             {
               [1,2,3,4].map((key, i) => (
                 <OrderCard key={i} ></OrderCard>
               ))
             }
           </ScrollView>
+
+          <View id='bemper' style={{ height: 80 }} ></View>
 
 
         </ScrollView>
@@ -59,29 +63,6 @@ const Home = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  scrollView: {
-    // backgroundColor: 'rgba(52, 52, 52, 0.8)',
-    backgroundColor: 'white',
-    flex: 1
-  },
-  text: {
-    fontSize: 42,
-  },
-  one: {
-    backgroundColor: 'red',
-    flexGrow: 1
-    // height: 50
-  },
-  tengah: {
-    backgroundColor: 'blue',
-    flex: 6
-    // height: 50
-  },
-  bwh: {
-    backgroundColor: 'cyan',
-    flexGrow: 1
-    // height: 50
   }
 });
 
