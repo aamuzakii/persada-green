11b4148ca951fe6c699700d009bce1e5968866a1
aamuzakii <aamuzakii@gmail.com>
2022-05-27 21:59:14 +0700

    make clean card, UI validated

--- src/components/home/OrderCard.jsx
+++ src/components/home/OrderCard.jsx
@@ -6,22 +6,27 @@ export default function OrderCard() {
   let screenHeight = Dimensions.get('window').height;
   let screenWidth = Dimensions.get('window').width;
 
+  let a = { fontSize: 11, color: '#b4b2cb', fontWeight: '500'}
+  let amei = { fontSize: 10, color: '#b4b2cb'}
+  let bolde = { fontWeight: 'bold', fontSize: 13 }
+
   return (
-    <View style={{ height: 190, width: screenWidth * 75 / 100, backgroundColor: 'white', margin: 5, padding: 10, borderRadius: 10, borderColor: '#f4f4f4', borderWidth: 3, shadowColor: "#000", shadowOffset: { width: 0, height: 3 }, shadowOpacity: 0.27, shadowRadius: 4.65, justifyContent: 'space-evenly' }} >
+    <View style={{ height: 150, width: screenWidth * 75 / 100, backgroundColor: 'white', margin: 5, padding: 10, borderRadius: 10, borderColor: '#f4f4f4', borderWidth: 3, shadowColor: "#000", shadowOffset: { width: 0, height: 3 }, shadowOpacity: 0.27, shadowRadius: 4.65, justifyContent: 'space-evenly' }} >
       <View id="atas" style={{ flexDirection: 'row', justifyContent: 'space-between' }} >
         <View style={{  }} >
-          <Text>Zaki</Text>
-          <Text style={{ fontSize: 12 }} >05 Mei 2022, 01: 33 PM</Text>
+          <Text style={bolde} >Zaki</Text>
+          <Text style={amei} >05 Mei 2022, 01:32 PM</Text>
         </View>
-        <View style={{ backgroundColor: 'pink', borderRadius: 10, padding: 0, justifyContent: 'center' }} ><Text style={{ color: 'red' }} >Belum Lunas</Text></View>
+        <View style={{ backgroundColor: '#fde7e9', borderRadius: 10, paddingHorizontal: 10, paddingBottom: 1, justifyContent: 'center', height: 25 }} ><Text style={{ color: 'red', fontSize: 10 }} >Belum Lunas</Text></View>
       </View>
       <View id="tengah" style={{ flexDirection: 'row', alignItems: 'center' }} >
-        <Image style={{ borderRadius: 12, marginRight: 10 }} source={{uri:"https://picsum.photos/200", width:50, height:50 }}/>
-        <Text>Honda Beat</Text>
+        <Image style={{ borderRadius: 12, marginRight: 10, marginVertical: 10 }} source={{uri:"https://picsum.photos/200", width:50, height:50 }}/>
+        <Text style={bolde} >Honda Beat</Text>
       </View>
-      <View id="bawah" style={{ flexDirection: 'row', justifyContent: 'space-between' }} >
-        <Text>Total Harga</Text>
-        <Text>Rp. 10.000</Text>
+      <View style={{ backgroundColor: '#b4b2cb', height: 1 }} ></View>
+      <View id="bawah" style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }} >
+        <Text style={amei} >Total Harga</Text>
+        <Text style={bolde} >Rp10.000</Text>
       </View>
     </View>
   );
