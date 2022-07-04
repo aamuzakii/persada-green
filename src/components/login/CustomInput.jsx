5365c386ef22314dec60bcf09e925bb1e597e3c6
aamuzakii <aamuzakii@gmail.com>
2022-07-05 06:00:34 +0700

    Refactor Login page

--- src/components/login/CustomInput.jsx
+++ src/components/login/CustomInput.jsx
@@ -2,14 +2,32 @@ import React from 'react'
 import { ImageBackground, StyleSheet, TextInput, View, TouchableHighlight, Text } from "react-native";
 import { passwordLogin } from '../../reducer/reducer'
 import { useDispatch } from 'react-redux';
-
+import { Checkbox } from 'react-native-paper';
 
 function CustomInput() {
 
-  const [email, setEmail] = React.useState('dev@persada.store')
-  const [password, setPassword] = React.useState('123')
+  const [email, setEmail] = React.useState('')
+  const [password, setPassword] = React.useState('')
+  const [checked, setChecked] = React.useState(false);
 
   const dispatch = useDispatch()
+
+  const handleTickCheckBox = () => {
+    setChecked(!checked)
+    setEmail('dev@persada.store')
+    setPassword('123')
+  }
+
+  const onChangeNumber =() => {
+
+  }
+
+  const touchableHighlightStyle = {
+    alignItems: "center",
+    backgroundColor: "#311b92",
+    padding: 10,
+    borderRadius: 10,
+  }
 
   const login = () => {
     dispatch(passwordLogin({ email, password }))
@@ -26,36 +44,16 @@ function CustomInput() {
 
   return (
     <>
-    <Text>Email</Text>
-    <TextInput
-          style={inputStyle}
-          // onChangeText={onChangeNumber}
-          value={email}
-          placeholder="Password"
-          // keyboardType="numeric"
-          selectionColor="red"
-        />
-            <Text>Password</Text>
-
-    <TextInput
-          style={inputStyle}
-          // onChangeText={onChangeNumber}
-          value={password}
-          placeholder="Masukan nomor WhatsApp"
-          keyboardType="numeric"
-          selectionColor="red"
-        />
-
-<TouchableHighlight onPress={login} >
-          <View style={{
-            alignItems: "center",
-            backgroundColor: "#311b92",
-            padding: 10,
-            borderRadius: 10,
-          }}>
-            <Text style={{ color: 'white', fontWeight: 'bold' }} >Masuk</Text>
-          </View>
-        </TouchableHighlight>
+      <Text>Email</Text>
+      <TextInput style={inputStyle} onChangeText={onChangeNumber} value={email} placeholder="Password" selectionColor="red" />
+      <Text>Password</Text>
+      <TextInput style={inputStyle} onChangeText={onChangeNumber} value={password} placeholder="Masukan nomor WhatsApp" selectionColor="red" />
+      <Checkbox status={checked ? 'checked' : 'unchecked'} onPress={handleTickCheckBox} color="blue"  />
+      <TouchableHighlight onPress={login} >
+        <View style={touchableHighlightStyle}>
+          <Text style={{ color: 'white', fontWeight: 'bold' }} >Masuk</Text>
+        </View>
+      </TouchableHighlight>
     </>
   )
 }
